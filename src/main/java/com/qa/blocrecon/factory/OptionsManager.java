1728544d51fd1e3bdf11c1f125fb5147efe41f08
nsramesh <rnallappan@vincilium.com>
2026-06-29 15:07:02 +0530

    fix

--- src/main/java/com/qa/blocrecon/factory/OptionsManager.java
+++ src/main/java/com/qa/blocrecon/factory/OptionsManager.java
@@ -2,19 +2,15 @@ package com.qa.blocrecon.factory;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class OptionsManager {
 
     public ChromeOptions getChromeOptions() {
+
         ChromeOptions options = new ChromeOptions();
 
-        // Basic options
-        boolean headless = Boolean.parseBoolean(System.getProperty("headless", "false"));
-        // options.addArguments("--disable-popup-blocking");
+        boolean headless = Boolean.parseBoolean(
+                System.getProperty("headless", "false"));
 
-        // Auto-detect Jenkins/CI environment or headless property
         if (headless) {
             options.addArguments("--headless=new");
             options.addArguments("--no-sandbox");
@@ -25,16 +21,7 @@ public class OptionsManager {
             options.addArguments("--start-maximized");
         }
 
-        // Suppress "Chrome is being controlled..." message
-        // options.setExperimentalOption("useAutomationExtension", false);
-        // options.setExperimentalOption("excludeSwitches", new String[] {
-        // "enable-automation" });
-
-        // // Optional: disable infobars as a fallback
-        // Map<String, Object> prefs = new HashMap<>();
-        // prefs.put("credentials_enable_service", false);
-        // prefs.put("profile.password_manager_enabled", false);
-        // options.setExperimentalOption("prefs", prefs);
+        System.out.println("Chrome Options: " + options.asMap());
 
         return options;
     }
