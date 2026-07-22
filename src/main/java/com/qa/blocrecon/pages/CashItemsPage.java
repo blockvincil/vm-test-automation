943e203b374a93080fd71cc6fc4214892d4dcabd
adi-vincil <adithyas@vincilium.com>
2026-07-22 15:02:32 +0530

    more waits

--- src/main/java/com/qa/blocrecon/pages/CashItemsPage.java
+++ src/main/java/com/qa/blocrecon/pages/CashItemsPage.java
@@ -150,9 +150,9 @@ public class CashItemsPage {
         rightClick(firstPendingRecord);
         eleUtil.doClick(approveOrRejectButton);
         eleUtil.doClick(approveButton, AppConstants.time10);
-        eleUtil.waitForElementToDisappear(pageLoader, AppConstants.time3, AppConstants.time10);
+//        eleUtil.waitForElementToDisappear(pageLoader, AppConstants.time3, AppConstants.time10);
         eleUtil.waitForElementToDisappear(cashItemsDataLoader, AppConstants.time3, AppConstants.time10);
-        waitUtil.waitFor(1);
+        waitUtil.waitFor(5);
 //        System.out.println("Ignore item done");
     }
 
